38fad99084a5116211c0e8d91601d06532cbb257
danielmoi <daniel@ampersandmoi.com>
2016-03-09 11:41:03 +1100

    add drawBall

--- src/js/app.js
+++ src/js/app.js
@@ -1,2 +1,27 @@
 var canvas = document.getElementById('myCanvas');
 var ctx = canvas.getContext('2d');
+
+var bX = canvas.width / 2;
+var bY = canvas.height - 30;
+
+var dbX = 2;
+var dbY = -2;
+
+var bRadius = 10;
+var bColor = 'tomato';
+
+var drawBall = function() {
+  ctx.beginPath();
+  ctx.arc(bX, bY, bRadius, 0, Math.PI * 2);
+  ctx.fillStyle = bColor;
+  ctx.fill();
+  ctx.closePath();
+};
+
+var drawEverything = function() {
+  drawBall();
+
+  var rAFid = requestAnimationFrame(drawEverything);
+};
+
+drawEverything();
